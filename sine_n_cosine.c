429d453e78ec874a1ad8319ecf826738a1ed4505
SuvorovDm-1 <dim-suv@mail.ru>
2024-10-31 03:28:20 +0300

    remove division from c_jk and add cosine coeffs

--- sine_n_cosine.c
+++ sine_n_cosine.c
@@ -5,10 +5,13 @@ double sin_minimax(double x) {
     uint64_t bits = *(uint64_t*)&x;
     int j = (bits >> 52) - 1023; 
     int k = (bits >> (52 - j)) & 0x7;
-    double cjk = (1.0 / (1ULL << j)) * (1.0 + (k >> 3));
+    int exponent = 1023 - j;
+    uint64_t tmp = (uint64_t)exponent << 52;
+    double cjk = (*(double*)&tmp) * (1.0 + (k >> 3));
 
     double R = x - cjk;
 
+    // Wolfram coeffs:
     double sin_R = ((((((-0.00018236 * R + 
                 -0.0000273543) * R + 
                 0.00835389) * R + 
@@ -17,6 +20,13 @@ double sin_minimax(double x) {
                 -1.81057e-7) * R + 
                 1.0) * R + 
                 -6.42047e-11;
+
+    double cos_R = ((((-0.00127554 * R + 
+                   -0.000163694) * R + 
+                   0.0417661) * R + 
+                   -0.0000296826) * R + 
+                   -0.499996) * R + 
+                   -2.28712e-7;
 
     return sin_R;
 }
